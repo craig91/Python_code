16de6abd4346c3c752b8d9c1d50478b8d62fcc1b
craig91 <craigdejean1@gmail.com>
2016-09-19 23:53:57 -0400

    better time format

--- min_hour_sec.py
+++ min_hour_sec.py
@@ -2,6 +2,10 @@ from datetime import datetime
 
 now = datetime.now()
 
-print now.hour 
-print now.minute 
-print now.second
+# print now.hour 
+# print now.minute 
+# print now.second
+
+#or 
+
+print '%s:%s:%s' % (now.hour, now.minute, now.second)
