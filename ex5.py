7e5dca5aed90e701393e1d31e6fcc82d94795ce6
craig91 <craigdejean1@gmail.com>
2016-09-24 08:54:08 -0400

    Python the hard way exercise 5

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Zed A. Shaw'
+my_age = 24
+my_height = 68
+my_weight = 165
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+print "Let's talk about %s" %  my_name
+print "He's %d inches tall" % my_height
+print "He's %d pounds heavy" % my_weight
+print "Acutally that's not that heavy" 
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
