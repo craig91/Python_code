79c29e58cb37c19e8e5e54861f7b597d7270517b
craig91 <craigdejean1@gmail.com>
2016-09-24 09:16:37 -0400

    update

---
+++ ex5.py
@@ -0,0 +1,17 @@
+myName = 'Zed A. Shaw'
+myAge = 24
+myHeight = 68
+myWeight = 165
+myEyes = 'brown'
+myTeeth = 'white'
+myHair = 'black'
+
+print "Let's talk about %s" %  myName
+print "He's %d inches tall" % myHeight
+print "He's %d pounds heavy" % myWeight
+print "Acutally that's not that heavy" 
+print "He's got %s eyes and %s hair" % (myEyes, myHair)
+print "His teeth are usually %s depending on the coffee." % myTeeth
+
+
+print "If I add %d, %d, and %d I get %d." % (myAge, myHeight, myWeight, myAge + myHeight + myWeight)
