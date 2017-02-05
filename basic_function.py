eeac47b213383b9cbbe679f37cd2accce8747f10
craig91 <craigdejean1@gmail.com>
2017-02-05 03:03:44 -0500

    basic function in python

---
+++ basic_function.py
@@ -0,0 +1,7 @@
+def checkDivisibility(a, b): # def is the function declaration key word for python.
+    if a % b == 0 :  # unlike javascript, there are no brackets
+        print "a is divisible by b"
+    else :
+        print "a is not divisible by b"
+
+checkDivisibility(6, 3)
