9d613a3497e660fe91e19a8396556f9feed168ff
craig91 <craigdejean1@gmail.com>
2017-04-18 19:15:52 -0400

    added string operations

--- basic_function.py
+++ basic_function.py
@@ -1,7 +1,7 @@
 def checkDivisibility(a, b): # def is the function declaration key word for python.
     if a % b == 0 :  # unlike javascript, there are no brackets
-        print "a is divisible by b"
+        print ("a is divisible by b")
     else :
-        print "a is not divisible by b"
+        print ("a is not divisible by b")
 
-checkDivisibility(6, 3)
+checkDivisibility(7, 3)
--- ex5.py
+++ ex5.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 myName = 'Zed A. Shaw'
 myAge = 24
 myHeight = 68
@@ -10,13 +10,13 @@ myHair = 'black'
 print "Let's talk about %s" %  myName
 print "He's %d inches tall" % myHeight
 print "He's %d pounds heavy" % myWeight
-print "Acutally that's not that heavy" 
+print "Acutally that's not that heavy"
 print "He's got %s eyes and %s hair" % (myEyes, myHair)
 print "His teeth are usually %s depending on the coffee." % myTeeth
 
 
 print "If I add %d, %d, and %d I get %d." % (myAge, myHeight, myWeight, myAge + myHeight + myWeight)
-=======
+
 my_name = 'Zed A. Shaw'
 my_age = 24
 my_height = 68
@@ -28,10 +28,9 @@ my_hair = 'black'
 print "Let's talk about %s" %  my_name
 print "He's %d inches tall" % my_height
 print "He's %d pounds heavy" % my_weight
-print "Acutally that's not that heavy" 
+print "Acutally that's not that heavy"
 print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
 print "His teeth are usually %s depending on the coffee." % my_teeth
 
 
 print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
->>>>>>> 7e5dca5aed90e701393e1d31e6fcc82d94795ce6
