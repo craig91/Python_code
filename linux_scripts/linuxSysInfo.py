fcb075590a0e6b3cd9f471b8936891d4a6fcc76a
craig91 <craigdejean1@gmail.com>
2022-10-05 20:18:53 -0400

    converted sub proccess calls into functions [systeminfoscript]

--- linux_scripts/linuxSysInfo.py
+++ linux_scripts/linuxSysInfo.py
@@ -3,13 +3,22 @@
 import subprocess
 
 # Command 1
-uname = "uname"
-uname_arg = "-a"
-print("Gathering system information with %s command:\n" % uname)
-subprocess.call([uname, uname_arg])
+def uname_func():
+    uname = "uname"
+    uname_arg = "-a"
+    print("Gathering system information with %s command:\n" % uname)
+    subprocess.call([uname, uname_arg])
 
 # Command 2
-diskspace = "df"
-diskspace_arg = "-h"
-print("Gathering diskspace information %s command:\n" % diskspace)
-subprocess.call([diskspace, diskspace_arg])
+def disk_func():
+    diskspace = "df"
+    diskspace_arg = "-h"
+    print("Gathering diskspace information %s command:\n" % diskspace)
+    subprocess.call([diskspace, diskspace_arg])
+
+# Main function that call other functions
+def main():
+    uname_func()
+    disk_func()
+
+main()
