bb0dffc42512e9e01942bfb20c1750f7ba6fb42f
craig91 <craigdejean1@gmail.com>
2022-10-18 11:39:34 -0400

    added attributes to new child class

--- basics/class_inheritance.py
+++ basics/class_inheritance.py
@@ -22,14 +22,29 @@ class medium_user:
 
 
 class medium_subscriber(medium_user):
-    pass
+    def __init__(self, s_name, s_surname, s_username, s_email, begindate, type_membership, payment_method):
+        super().__init__(s_name, s_surname, s_username, s_email)
+        
+        self.Begindate = begindate
+        self.Type_membership = type_membership
+        self.Payment_method = payment_method
+
+tim = medium_subscriber('Tim', 'Johnson', 'tim-johnson', 'timjohnson@gmail.com', 'March 2019', 'monthly', 'venmo')
+print(tim.Username)
+print(tim.Begindate)
+tim.read()
+tim.clap()
+tim.is_member()
 
 
-jeff = medium_subscriber("Jeff", "Grimm", "jeff-grimm", "jeffgrim@gmail.com")
-print(jeff.Name)
-jeff.read()
-jeff.clap()
-jeff.is_member()
+# this functions below need to call the child classes new attributes in order to work, otherwise you will get an error
+
+
+# jeff = medium_subscriber("Jeff", "Grimm", "jeff-grimm", "jeffgrim@gmail.com")
+# print(jeff.Name)
+# jeff.read()
+# jeff.clap()
+# jeff.is_member()
 
 
 # craig = medium_user('Craig', 'Dejean', 'craig-dejean', 'cdejeantsuno@icloud.com', False)
