f38739045496b2e99f657bd8fc25154132a21de6
craig91 <craigdejean1@gmail.com>
2022-10-18 11:25:23 -0400

    added child class with basic inheritance

--- basics/class_inheritance.py
+++ basics/class_inheritance.py
@@ -21,8 +21,19 @@ class medium_user:
             print('You are a member')
 
 
-craig = medium_user('Craig', 'Dejean', 'craig-dejean', 'cdejeantsuno@icloud.com', False)
-print(craig.Username)
-craig.read()
-craig.clap()
-craig.is_member()
+class medium_subscriber(medium_user):
+    pass
+
+
+jeff = medium_subscriber("Jeff", "Grimm", "jeff-grimm", "jeffgrim@gmail.com")
+print(jeff.Name)
+jeff.read()
+jeff.clap()
+jeff.is_member()
+
+
+# craig = medium_user('Craig', 'Dejean', 'craig-dejean', 'cdejeantsuno@icloud.com', False)
+# print(craig.Username)
+# craig.read()
+# craig.clap()
+# craig.is_member()
