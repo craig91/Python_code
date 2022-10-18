abee84d7412e41ebbd5c49c7cf97b13414fb96a1
craig91 <craigdejean1@gmail.com>
2022-10-18 11:22:17 -0400

    added medium class

--- basics/class_inheritance.py
+++ basics/class_inheritance.py
@@ -1,1 +1,28 @@
 # class inheritance using the super() function
+
+class medium_user:
+    def __init__(self, name, surname, username, email, subscriber=True):
+        self.Name = name
+        self.Surname = surname
+        self.Username = username
+        self.Email = email
+        self.Subscriber = subscriber
+
+    def read(self):
+        print("{} {} is reading a story".format(self.Name, self.Surname))
+
+    def clap(self):
+        print("{} {} clapped a story".format(self.Name, self.Surname))
+
+    def is_member(self):
+        if (self.Subscriber==False):
+            print("Become a member to get unlimited stories on Medium")
+        else:
+            print('You are a member')
+
+
+craig = medium_user('Craig', 'Dejean', 'craig-dejean', 'cdejeantsuno@icloud.com', False)
+print(craig.Username)
+craig.read()
+craig.clap()
+craig.is_member()
