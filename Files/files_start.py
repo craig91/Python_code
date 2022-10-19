de3563db50a07fa4ce894bf1e037babff5846815
craig91 <craigdejean1@gmail.com>
2022-10-19 11:45:14 -0400

    tested function on linux, success

--- Files/files_start.py
+++ Files/files_start.py
@@ -1,11 +1,11 @@
 def main():
-    f = open("textfile.txt", "w+")
+    f = open("myile.txt", "w+")
 
     for i in range(10):
         f.write("This is line %d\r\n" % (i+1))
     f.close()
 
-    f = open("textfile.txt", "r")
+    f = open("myile.txt", "r")
     if f.mode == "r":
         fl = f.readlines()
         for x in fl:
