58ba0fa53a17bb63efb93e61c58bd56ce1d516a4
craig91 <craigdejean1@gmail.com>
2023-10-27 10:50:22 -0400

    added binary and hex funtions to a value

--- ztm/num_float.py
+++ ztm/num_float.py
@@ -1,12 +1,25 @@
-print(type(6))
-print(type(2-4))
-print(type(2*4))
-print(type(2/4))  #0.5
+#Fundanental Data Types (int and float)
+#floating point number is a number with a decimal point
+# A float takes up more memory than an integer 
+# Integers --> Whole numbers were used
+# print(2 + 4)
+# print (2 - 4)
+# print (2 * 4)
+# print (2 / 4)
+
+#checking the type of a value
+# print(type(3 * 9))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4)) #returns float because the result is a decimal number
 
 
-print(type(0))
 
-print(2 ** 3)
-print(5 // 4)
-print(6 % 4)
+# print(type(0.00001))
+val = 6
+# print(bin(val))
+# print(id(val))
+# print(hex(id(val)))
 
+print(2 ** 8)
+print(2 // 4)
