af7e4c070d47d83bf16cccdc561fc7748bfbd9f0
craig91 <craigdejean1@gmail.com>
2023-10-27 13:04:25 -0400

    string format and un-commented some functions

--- ztm/num_float.py
+++ ztm/num_float.py
@@ -2,28 +2,28 @@
 #floating point number is a number with a decimal point
 # A float takes up more memory than an integer 
 # Integers --> Whole numbers were used
-# print(2 + 4)
-# print (2 - 4)
-# print (2 * 4)
-# print (2 / 4)
+print(2 + 4)
+print (2 - 4)
+print (2 * 4)
+print (2 / 4)
 
 #checking the type of a value
-# print(type(3 * 9))
-# print(type(2 - 4))
-# print(type(2 * 4))
-# print(type(2 / 4)) #returns float because the result is a decimal number
+print(type(3 * 9))
+print(type(2 - 4))
+print(type(2 * 4))
+print(type(2 / 4)) #returns float because the result is a decimal number
 
 
 
-# print(type(0.00001))
-# val = 6
-# print(bin(val))
-# print(id(val))
-# print(hex(id(val)))
+print(type(0.00001))
+val = 6
+print(bin(val))
+print(id(val))
+print(hex(id(val)))
 
-# print(2 ** 8)
-# print(2 // 4)
-# print(5%4)
+print(2 ** 8)
+print(2 // 4)
+print(5%4)
 
 # math functions
 print(round(3.1))
--- ztm/strings.py
+++ ztm/strings.py
@@ -22,12 +22,32 @@ print(full_name)
 
 # Type conversion
 
-print(type(str(100))) # Converts an int into a str type
+# print(type(str(100))) # Converts an int into a str type
 
-print(type(int(str(100)))) # The conversion starts from the right most function:  type() <-- int() <-- str() so the output will be an int
+# print(type(int(str(100)))) # The conversion starts from the right most function:  type() <-- int() <-- str() so the output will be an int
 # the function directly above is the same as the one below
 a = str(100)
 b = int(a)
 c = type(b)
 
 print(c)
+
+
+# Escape Sequences
+# weather = " \t It\'s \"kind of\" Sunny \n hope you have a good day!"
+# print(weather)
+
+# Formatted Strings
+name = 'Johnny'
+age = 55
+
+# Without format
+print('hi ' + name + '. You are ' + str(age) + ' years old')
+
+# With format
+print(f'hi {name}. You are {age} years old')
+
+# This f is a new feature of python 3
+
+# python2 style
+print('hi {}. You are {} years old'.format(name, age))
