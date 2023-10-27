a019cfdad66b26d6499cf1e34bf9ee02543f95c5
craig91 <craigdejean1@gmail.com>
2023-10-27 11:57:16 -0400

    Statements v. Expressions

---
+++ ztm/var.py
@@ -0,0 +1,8 @@
+iq = 100 # --> This is a statement
+user_age = iq / 5 # --> This entire line is a statment while iq / 5 by itself is an expression
+
+# a,b,c = 1,2,3
+# print(a)
+# print(b)
+# print(c)
+
