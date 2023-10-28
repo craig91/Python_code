89fc519dd3e819e971a07897ef5011db98695996
craig91 <craigdejean1@gmail.com>
2023-10-28 17:36:23 -0400

    added list methods

--- ztm/list.py
+++ ztm/list.py
@@ -26,13 +26,23 @@ matrix = [
           [7,8,9] 
 ]
 
-print("Sub Array: ", matrix[0])
-print("Sub Array: ", matrix[1])
-print("Sub Array: ", matrix[2])
+# print("Sub Array: ", matrix[0])
+# print("Sub Array: ", matrix[1])
+# print("Sub Array: ", matrix[2])
 
-print(matrix[0][1])
+# print(matrix[0][1])
 
+# list methods
+basket = [1,2,3,4,5]
+# print(len(basket))
 
+ # add to list
 
- 
+new_list = basket.extend([100,101])
+# print(basket)
+# print(new_list) #does not produce a value, so if you want one, assign basket to a new_list variable
+
+# remove
+new_list = basket.clear()
+print(basket)
  
