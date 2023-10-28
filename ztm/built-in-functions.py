69163d3610749d8891a32fc6e4523bf872f670b8
craig91 <craigdejean1@gmail.com>
2023-10-28 14:35:02 -0400

    added string maniplulation operations using string methods

--- ztm/built-in-functions.py
+++ ztm/built-in-functions.py
@@ -1,5 +1,18 @@
-greet = 'helllloooo'
-print(greet[0:9])
+# greet = 'helllloooo'
+# print(len(greet))
+# print(greet[0:9])
+
+quote = 'to be or not to be'
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.lower())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
+
+quote2 = quote.replace('be', 'me')
+print(quote2)
 
 
 
