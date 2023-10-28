d7309e079f15b2264d1f593661b32ad978ec6e3f
craig91 <craigdejean1@gmail.com>
2023-10-28 17:56:28 -0400

    added dictionary operations

---
+++ ztm/dictionary.py
@@ -0,0 +1,33 @@
+this_dict = {
+    "brand": "subaru",
+    "model": "impreza",
+    "year": 2001
+}
+
+# print(this_dict)
+# print(this_dict["brand"])
+
+# print(len(this_dict))
+# print(type(this_dict))
+
+# key = this_dict.keys()
+# print(key)
+
+# this_dict["color"] = "red"
+# print(this_dict)
+
+# key = this_dict.values()
+# print(key)
+
+# key = this_dict.items()
+# print(key)
+
+# for x in this_dict.values():
+#     print(x)
+    
+
+# for x in this_dict.keys():
+#     print(x)
+
+for x, y in this_dict.items():
+    print(x,y)
