06f3ff9836e700f8cbdce4b4614267a7e31b00c8
craig91 <craigdejean1@gmail.com>
2023-10-28 17:23:05 -0400

    Added matrix operations

--- ztm/list.py
+++ ztm/list.py
@@ -12,10 +12,27 @@ amazon_cart = [
 # print(amazon_cart[0::2])
 
 amazon_cart[0] = 'laptop'
+
 new_cart = amazon_cart
 new_cart[0] = 'gum'
-print(new_cart)
-print(amazon_cart)
+# print(new_cart)
+# print(amazon_cart)
+
+#Matrix
+
+matrix = [ 
+          [1,2,3], 
+          [2,4,6], 
+          [7,8,9] 
+]
+
+print("Sub Array: ", matrix[0])
+print("Sub Array: ", matrix[1])
+print("Sub Array: ", matrix[2])
+
+print(matrix[0][1])
+
+
 
  
  
