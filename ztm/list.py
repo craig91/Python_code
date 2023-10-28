4ad47c6367b4e01efad44125e3348d4aca471052
craig91 <craigdejean1@gmail.com>
2023-10-28 16:24:39 -0400

    added list slicing and copying operations

---
+++ ztm/list.py
@@ -0,0 +1,21 @@
+amazon_cart = [
+    'notebooks', 
+    'sunglasses',
+    'toys',
+    'grapes'
+]
+
+# print(amazon_cart)
+
+ # List slicing
+# print(amazon_cart[0:2])
+# print(amazon_cart[0::2])
+
+amazon_cart[0] = 'laptop'
+new_cart = amazon_cart
+new_cart[0] = 'gum'
+print(new_cart)
+print(amazon_cart)
+
+ 
+ 
--- ztm/random.py
+++ ztm/random.py
@@ -5,10 +5,23 @@ current_year = date.today()
 current_year = current_year.year
 
 
-birth_year = int(input('What year were you born? '))
+# birth_year = int(input('What year were you born? '))
 
-age = current_year - birth_year
+#age = current_year - birth_year
 
-your_birth_year = f'Your age is {age}'
-print(your_birth_year)
+#your_birth_year = f'Your age is: {age}'
+# print(your_birth_year)
 
+
+
+# Password Checker
+
+# ask for username 
+username = input("What is your username?: ")
+
+# ask for password
+password = input("What is your password?: ")
+password_length = len(password)
+obfuscate = '*' * password_length
+
+print(f'{username}, your password {obfuscate} is {password_length} characters long')
