359612ea5df26206569ac25d7b545e6c8f1ff189
craig91 <craigdejean1@gmail.com>
2023-12-21 22:33:24 -0500

    added a function that demonstrates positional arguments

--- Book/function_basic.py
+++ Book/function_basic.py
@@ -2,7 +2,7 @@ def greet_user():
     """Display a simple greeting""" ## This is a Docstring, which describes what the function does. D
     print("Hello!")
 
-greet_user()
+# greet_user()
 
 
 ## Passing info to the function
@@ -10,9 +10,22 @@ greet_user()
 def greet_user2(username):
     print(f"Hello, {username.title()}!")
 
-greet_user2('minami')
+# greet_user2('minami')
 
 def display_message():
     print("I am learning python basics")
 
-display_message()
+# display_message()
+
+def favorite_book(title):
+    print(f"One of my favorite books is {title}")
+
+# favorite_book("Zero to one")
+
+## Positional Argument demostration
+def describe_pet(animal_type, pet_name):
+    print(f"\nI have a {animal_type}")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('cat', 'ame')
+
