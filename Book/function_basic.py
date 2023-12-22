d8399b0537aefc5cfb1e98574625774ab60b9f47
craig91 <craigdejean1@gmail.com>
2023-12-21 22:40:06 -0500

    added a keyword arg function call

--- Book/function_basic.py
+++ Book/function_basic.py
@@ -28,4 +28,4 @@ def describe_pet(animal_type, pet_name):
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 describe_pet('cat', 'ame')
-
+describe_pet(pet_name='jacob', animal_type='alpaca') ## keyword arg ignores order
