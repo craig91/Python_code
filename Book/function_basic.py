52f9d413fb3728d9fbb2dd20381d4e68fb9b9c31
craig91 <craigdejean1@gmail.com>
2023-12-22 21:15:57 -0500

    added additional practice positional arg functions

--- Book/function_basic.py
+++ Book/function_basic.py
@@ -27,12 +27,33 @@ def describe_pet(animal_type, pet_name):
     print(f"\nI have a {animal_type}")
     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
-describe_pet('cat', 'ame')
-describe_pet(pet_name='jacob', animal_type='alpaca') ## keyword arg ignores order
+# describe_pet('cat', 'ame')
+# describe_pet(pet_name='jacob', animal_type='alpaca') ## keyword arg ignores order
 
 def describe_pet2(pet_name2, animal_type2='dog'):
     print(f"\nI have a {animal_type2}")
     print(f"His name is {pet_name2.title()}")
 
-describe_pet2("Jimmy")
-describe_pet2("mr.oink", animal_type2='pig', ) ## when using a default value, positional args apply in this case.
+# describe_pet2("Jimmy")
+# describe_pet2("mr.oink", animal_type2='pig', ) ## when using a default value, positional args apply in this case.
+
+def make_shirt(size, message):
+    print(f"\nYour shirt size is: {size}")
+    print(f"And the message is: {message}")
+
+def make_shirt2(size="Large", message="I love python"):
+    print(f"\nYour shirt size is: {size}")
+    print(f"The message on the shirt says: {message}")
+
+
+
+make_shirt("Large", "I am too sexy for this shirt")
+make_shirt(message="I am a baseball fan", size="Medium")
+make_shirt2()
+
+def describe_city(city, country):
+    print(f"\n{city} is in {country}")
+
+describe_city("Reykjavik", "Iceland")
+describe_city("Cape Town", "South Africa")
+describe_city("Moscow", "Russia")
