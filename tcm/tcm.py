01d7fea8b6761c2ab79b7b5a541d828b1a638474
craig91 <craigdejean1@gmail.com>
2023-12-28 20:28:17 -0500

    added list() constructor example

--- tcm/tcm.py
+++ tcm/tcm.py
@@ -1,4 +1,28 @@
 movies = ["when Harry met sally", "the hangover", "the perks of being a wallflower", "the matrix"]
 
-print(movies[1:3]);
-# print(movies[1]);
+# print(movies[1:3]);
+# print(movies[1:]);
+# print(movies[:1]);
+
+# print(movies.append('benjamin button'));
+# print(movies)
+
+# print(movies.insert(3, 'Hustle and Flow'));
+# print(movies)
+
+# movies.pop()
+# print(movies)
+
+
+minamiFavoriteMovies = ['kimetsu no yaiba', 'flubber']
+ourFavoriteMovies = movies + minamiFavoriteMovies;
+# print(ourFavoriteMovies)
+
+grades = [['Bob', 82], ['Alice',90], ['Jeff', 73]];
+bobs_grade = grades[0][1];
+print(bobs_grade)
+grades[0][1] = 83;
+print(grades)
+
+thisList = list(('apple', 'banana', 'cherry'));
+print(thisList)
