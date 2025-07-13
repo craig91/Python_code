0f892520142b37a7fdbd7599b56f84e713b0e3ac
craig91 <craigdejean1@gmail.com>
2025-07-13 19:19:01 +0900

    Added new linux setup script

--- sec/input.py
+++ sec/input.py
@@ -1,2 +1,11 @@
-name = input("Enter your name: ")
-print("Hello " + name+ "!")
+#MATH formula = GigaBytes(GB) = Bytes(B) / 1,073741,824
+
+GIGABYTE_IN_BYTES = 1073741824
+
+def bytes_to_gb(byte_count):
+   gb = round(int(byte_count) / GIGABYTE_IN_BYTES, 2)
+   return f"{gb:.2f} GB"
+
+
+result = bytes_to_gb(5000000000)
+print(result)
