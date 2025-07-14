ae288225ba8f94f543eb8afc648a799c9be587ea
craig91 <craigdejean1@gmail.com>
2025-07-14 11:21:46 +0900

    added solved challenge

---
+++ Challenges/banjo.py
@@ -0,0 +1,14 @@
+def are_you_playing_banjo(name):
+    if (name[0] == "R" or name[0] == "r"):
+        return name + " plays banjo"
+    else:
+        return name + " does not play banjo"
+    return name
+
+
+are_you_playing_banjo("craig")
+are_you_playing_banjo("ririri")
+are_you_playing_banjo("Rikku")
+are_you_playing_banjo("Minami")
+are_you_playing_banjo("Adam")
+are_you_playing_banjo("Ringo")
